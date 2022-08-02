979a86ccbbff1ff80d5e5f151dcbeb319ff1e65a
ridalgo- <ridalgo-@student.42sp.org.brr>
2022-08-02 21:51:01 +0200

    press button stage tests

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,18 +6,28 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 20:45:39 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/08/02 17:08:18 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/08/02 19:15:14 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# define WINDOW_HEI 600
-# define WINDOW_WID 500
+# define WINDOW_HEIGHT 600
+# define WINDOW_WIDTH 500
+# define MLX_ERROR 1
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_data;
 
 # include <stdlib.h>
 # include <mlx.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <stdio.h>
 # include "../libraries/42_libft/libft.h"
 
 #endif
--- sources/fdf.c
+++ sources/fdf.c
@@ -6,29 +6,87 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 20:44:25 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/08/02 17:11:23 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/08/02 19:15:34 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-int main(int argcl, char **argv)
+int	handle_no_event(void *data)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	/* This function needs to exist, but it is useless for the moment */
+	return (0);
+}
 
-	mlx_ptr = mlx_init();
-	if (mlx_ptr == NULL)
-		return (1);
-	win_ptr = mlx_new_window(mlx_ptr, WINDOW_HEI, WINDOW_WID, "My first window!");
-	if (win_ptr == NULL)
+// int	handle_input(int keysym, t_data *data)
+// {
+// 	if (keysym == XK_Escape)
+// 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+// 	return (0);
+// }
+
+// int	main(void)
+// {
+// 	t_data	data;
+
+// 	data.mlx_ptr = mlx_init();
+// 	if (data.mlx_ptr == NULL)
+// 		return (MLX_ERROR);
+// 	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
+// 			"My first window!");
+// 	if (data.win_ptr == NULL)
+// 	{
+// 		free(data.win_ptr);
+// 		return (MLX_ERROR);
+// 	}
+
+// 	/* Setup hooks */ 
+// 	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+// 	mlx_key_hook(data.win_ptr, &handle_input, &data);
+
+// 	mlx_loop(data.mlx_ptr);
+
+// 	/* we will exit the loop if there's no window left, and execute this code */
+// 	mlx_destroy_display(data.mlx_ptr);
+// 	free(data.mlx_ptr);
+// }
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+
+	printf("Keypress: %d\n", keysym);
+	return (0);
+}
+
+int	handle_keyrelease(int keysym, void *data)
+{
+	printf("Keyrelease: %d\n", keysym);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+	if (data.win_ptr == NULL)
 	{
-		free(win_ptr);
-		return (1);
+		free(data.win_ptr);
+		return (MLX_ERROR);
 	}
-	while (1)
-		;
-	mlx_destroy_window(mlx_ptr, win_ptr);
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+
+	/* Setup hooks */ 
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data); /* ADDED */
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data); /* CHANGED */
+
+	mlx_loop(data.mlx_ptr);
+
+	/* we will exit the loop if there's no window left, and execute this code */
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
