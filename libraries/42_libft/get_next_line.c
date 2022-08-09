40e046290b2ec22469351b43436943925e8ab20c
ridalgo- <ridalgo-@student.42sp.org.brr>
2022-08-09 17:05:09 +0200

    automatic commit [Make Git]

---
+++ libraries/42_libft/get_next_line.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 22:25:47 by ridalgo-          #+#    #+#             */
+/*   Updated: 2022/05/12 19:52:49 by ridalgo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*cat;
+	size_t	len;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	cat = (char *)malloc(sizeof(char) * (len + 1));
+	if (!cat)
+		return (NULL);
+	cat[len] = 0;
+	while (s1[i])
+	{
+		cat[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		cat[i] = s2[j];
+		j++;
+		i++;
+	}
+	cat[i] = 0;
+	return (cat);
+}
+
+static void	ft_freethis(char **str)
+{
+	if (str[0])
+	{
+		free(str[0]);
+		str[0] = NULL;
+	}
+}
+
+static char	*ft_badsplit(char **line_static)
+{
+	int		i;
+	char	*line;
+	char	*temp;
+
+	i = 0;
+	if (!line_static[0])
+		return (NULL);
+	while (line_static[0][i] != '\n' && line_static[0][i] != '\0')
+		i++;
+	line = ft_substr(line_static[0], 0, (i + 1));
+	temp = ft_strdup(line_static[0]);
+	ft_freethis(line_static);
+	line_static[0] = ft_substr(temp, (i + 1), ft_strlen(temp));
+	ft_freethis(&temp);
+	if (!ft_strchr(line, '\n'))
+	{
+		if (!ft_strlen(line))
+			ft_freethis(&line);
+		ft_freethis(line_static);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*found;
+	char		*temp;
+	static char	*line_static;
+	int			file_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	found = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	file_read = read (fd, found, BUFFER_SIZE);
+	while (file_read > 0)
+	{
+		found[file_read] = 0;
+		if (line_static == NULL)
+			line_static = ft_strdup(found);
+		else
+		{
+			temp = ft_strjoin(line_static, found);
+			ft_freethis(&line_static);
+			line_static = temp;
+		}
+		if (ft_strchr(line_static, '\n'))
+			break ;
+		file_read = read (fd, found, BUFFER_SIZE);
+	}
+	return (ft_freethis(&found), ft_badsplit(&line_static));
+}
---
+++ libraries/42_libft/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 22:46:38 by ridalgo-          #+#    #+#             */
+/*   Updated: 2022/08/04 22:24:04 by ridalgo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFFER_SIZE 4096
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
