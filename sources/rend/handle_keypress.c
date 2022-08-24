103c80f482bf892bc18d26ebd1d6a40ccb675be3
Vanessa <vanesasilva_sc@hotmail.com>
2022-08-24 19:48:24 +0200

    automatic commit [Make Git]

--- sources/rend/handle_keypress.c
+++ sources/rend/handle_keypress.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:53:07 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/08/22 20:51:21 by coder            ###   ########.fr       */
+/*   Updated: 2022/08/24 19:36:24 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	handle_keypress(int keypress, t_data *data)
 {
-	printf("%d\n", keypress);
 	if (keypress == 65307)
 	{
 		mlx_destroy_image(data->mlx_ptr, data->img.mlx_img);
