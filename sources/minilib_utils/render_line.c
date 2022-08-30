de208305c2408ac0bb09579c9abbdfe0b284ef65
ridalgo- <ridalgo-@student.42sp.org.brr>
2022-08-31 00:10:38 +0200

    lines

--- sources/minilib_utils/render_line.c
+++ sources/minilib_utils/render_line.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:51:22 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/08/25 21:35:49 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/08/31 00:08:58 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,28 @@ int render_line(t_img *img, t_data *data)
 	int	j;
 	int	x;
 	int	y;
-
+	int dots;
+	
 	i = 0;
 	while (i < data->rows)
 	{
 		j = 0;
 		while (j < data->cols)
 		{
-			x = (WINDOW_WIDTH / 2) - (data->cols * data->scalej / 2) + (j * data->scalej);
-			y = (WINDOW_HEIGHT / 2) - (data->rows * data->scalei  / 2) + (i *  data->scalei);
-			img_pix_put(img, x, y, data->dot[i][j].color);
+			dots = 0;
+			while (dots < data->scalej && j < data->cols - 1)
+			{
+				x = (WINDOW_WIDTH / 2) - (data->scalej * data->cols / 2) + (j * data->scalej);
+				y = (WINDOW_HEIGHT / 2) - (data->scalei * data->rows  / 2) + (i *  data->scalei);
+				img_pix_put(img, x + dots++, y, data->dot[i][j].color);
+			}
+			dots = 0;
+			while (dots < data->scalei && i < data->rows - 1)
+			{
+				x = (WINDOW_WIDTH / 2) - (data->scalej * data->cols / 2) + (j * data->scalej);
+				y = (WINDOW_HEIGHT / 2) - (data->scalei * data->rows  / 2) + (i *  data->scalei);
+				img_pix_put(img, x, y + dots++, data->dot[i][j].color);
+			}
 			j++;
 		}
 		i++;
