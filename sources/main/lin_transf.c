92c4dddf841f4a78ef1efaeb203772ad78f15509
renanguillen <rigcpj@gmail.com>
2022-09-02 00:01:25 +0200

    automatic commit [Make Git]

--- sources/main/lin_transf.c
+++ sources/main/lin_transf.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 17:01:02 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/09/01 17:29:03 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/09/02 00:00:54 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	lin_transf(t_data *data, int i, int j)
 {
-	data->dot[i][j].x = ((WINDOW_HEIGHT / 2)  - (data->cols * data->scalei));
-	data->dot[i][j].x -= (data->dot[i][j].z);
-	data->dot[i][j].x += (i * data->scalei);
-	data->dot[i][j].x += (j * data->scalei);
-	data->dot[i][j].y = ((WINDOW_WIDTH / 1.2) - (data->rows * data->scalej));
-	data->dot[i][j].y -= (i * data->scalej);
-	data->dot[i][j].y += (j * data->scalej);
+	data->dot[i][j].y = ((WINDOW_HEIGHT / 2)  - (data->cols * data->scalei));
+	data->dot[i][j].y -= (data->dot[i][j].z * 200 / data->diagonal);
+	data->dot[i][j].y += (i * data->scalei);
+	data->dot[i][j].y += (j * data->scalei);
+	data->dot[i][j].x = ((WINDOW_WIDTH / 1.2) - (data->rows * data->scalej));
+	data->dot[i][j].x -= (i * data->scalej);
+	data->dot[i][j].x += (j * data->scalej);
 }
--- sources/minilib_utils/render_line.c
+++ sources/minilib_utils/render_line.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:51:22 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/09/01 01:50:22 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/09/01 23:27:45 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int render_line(t_img *img, t_data *data)
 		j = 0;
 		while (j < data->cols)
 		{
-			x = data->dot[i][j].x;
-			y = data->dot[i][j].y;
+			x = data->dot[i][j].y;
+			y = data->dot[i][j].x;
 			if (x < 0 || y < 0 || j > WINDOW_WIDTH || x > WINDOW_HEIGHT)
 				return (0);
 			img_pix_put(img, y, x, data->dot[i][j].color);
