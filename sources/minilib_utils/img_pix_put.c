ce1731f3a5d6af93bd9547e1989ed26699fb6309
renanguillen <rigcpj@gmail.com>
2022-09-03 00:36:51 +0200

    test

--- sources/minilib_utils/img_pix_put.c
+++ sources/minilib_utils/img_pix_put.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:49:24 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/09/03 00:18:34 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/09/03 00:36:20 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 	char	*pixel;
 	int		i;
 
-	// if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT )
-	// 	return ;
+	if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT )
+		return ;
 	i = img->bpp - 8;
 	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
 	while (i >= 0)
