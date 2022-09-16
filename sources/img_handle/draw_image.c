fb4849a68a32d4c4d49f7a4883ea098cadbb4af1
renanguillen <rigcpj@gmail.com>
2022-09-16 23:06:46 +0200

    automatic commit [Make Git]

--- sources/img_handle/draw_image.c
+++ sources/img_handle/draw_image.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:56:25 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/09/16 22:48:50 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/09/16 23:00:47 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	draw_image(t_data *data)
 	if (data->win_ptr == NULL)
 		return (1);
 	if (data->key.press)
+	{
 		clear_window(&data->img);
+		data->key.press = 0;
+	}
 	bresenham_line_drawing(data);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
 		data->img.mlx_img, 0, 0);
--- sources/win_handle/handle_keypress.c
+++ sources/win_handle/handle_keypress.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:54:39 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/09/16 22:33:17 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/09/16 23:03:43 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,25 @@
 static int	handle_arrows(int keymask, t_key *key)
 {
 	if (keymask == XK_Left)
+	{
 		key->travelx -= 30;
+		key->press = 1;
+	}
 	else if (keymask == XK_Right)
+	{
 		key->travelx += 30;
+		key->press = 1;
+	}
 	else if (keymask == XK_Up)
+	{
 		key->travely -= 30;
+		key->press = 1;
+	}
 	else if (keymask == XK_Down)
+	{
 		key->travely += 30;
-	key->press = 1;
+		key->press = 1;
+	}
 	return (0);
 }
 
